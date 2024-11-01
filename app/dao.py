7cc651c6073cc53a49b94d0275f6c69787371faf
thien0709 <thien070904@gmail.com>
2024-11-01 08:18:51 +0700

    update

---
+++ app/dao.py
@@ -0,0 +1,7 @@
+def load_categories():
+    return [
+        {"id": 1, "name": "Mobile"},
+        {"id": 2, "name": "Tablet"},
+        {"id": 3, "name": "PC"},
+        {"id": 4, "name": "PCB"}
+        ]
